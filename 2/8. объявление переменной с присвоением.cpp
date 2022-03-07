d236c23926508507b5e7eb9e812d0c86604f703f
Shmulik <52025854+Masortim@users.noreply.github.com>
2022-03-08 00:14:41 +0700

    Update 8. объявление переменной с присвоением.cpp

--- 2/8. объявление переменной с присвоением.cpp
+++ 2/8. объявление переменной с присвоением.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <Windows.h>
+#include <string>
 using namespace std;
 
 int main()
 {
-	int number = 2, x=7;              // целочисленные
-	float length = 2.5;               // вещественные
-	bool isEven = true;               // логическая переменная
-	char symbol = '*', letter = 'A';  // символ
+	int number = 2, x=7;              	// целочисленные
+	float length = 2.5;               	// вещественные
+	bool isEven = true;               	// логическая переменная
+	char symbol = '*', letter = 'A';  	// символ
+	string str = "apple";			// строка, для её работы желательно подключить библиотеку #include <string>
 	cout << number << endl;
 	cout << x << endl;
 	cout << length << endl;
 	cout << isEven << endl;
 	cout << symbol << endl;
 	cout << letter << endl;
+	cout << str << endl;
 	system("pause");
 	return 0;
 }
