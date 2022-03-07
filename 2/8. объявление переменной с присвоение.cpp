26db1a666fc903433ebd1c28ac09d5c07ff0bd03
Shmulik <52025854+Masortim@users.noreply.github.com>
2022-03-08 00:09:24 +0700

    Create 8. объявление переменной с присвоение.cpp

---
+++ 2/8. объявление переменной с присвоение.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	int number = 2, x=7;              // целочисленные
+	float length = 2.5;               // вещественные
+	bool isEven = true;               // логическая переменная
+	char symbol = '*', letter = 'A';  // символ
+	cout << number << endl;
+	cout << x << endl;
+	cout << length << endl;
+	cout << isEven << endl;
+	cout << symbol << endl;
+	cout << letter << endl;
+	system("pause");
+	return 0;
+}
