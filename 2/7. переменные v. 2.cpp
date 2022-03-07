7a0fcba97bbee0a3ded232316c3447ff3214e351
Shmulik <52025854+Masortim@users.noreply.github.com>
2022-03-07 23:49:24 +0700

    Update 7. переменные v. 2.cpp

--- 2/7. переменные v. 2.cpp
+++ 2/7. переменные v. 2.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
-	int number;
+	int number;		// пустая переменная целого числа (int)
 	number = 2;
 	cout << number << endl;
 	system("pause");
 	return 0;
 }
+
+/* В некоторых языках, например в Python, этот вариант недопустим.
+Невозможно сделать никаких математических операций, если пременная пуста */
